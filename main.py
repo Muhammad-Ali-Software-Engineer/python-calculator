99dba2067253da4801b7ce2d778e9811ecd9497b
Muhammad Ali <MuhammadAliOfficial75@gmail.com>
2026-02-07 01:32:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -34,33 +34,6 @@ while True:
     if option == 0:
         print("Program terminated.")
         break
-    else:
-        a = float(input("Enter number 1: "))
-        b = float(input("Enter number 2: "))
-        if option == 1:
-            print(f"Sum of {a} & {b} = {a+b}")
-        elif option == 2:
-            print(f"Difference of {a} & {b} = {a-b}")
-        elif option == 3:
-            print(f"Multiply of {a} & {b} = {a*b}")
-        elif option == 4:
-            print(f"Division of {a} & {b} = {a/b}")
-        else:
-            print("please choose right option.") 
-'''
-# ==========================================================
-'''
-while True: 
-    print("======= Calculator =======")
-    print("1. Addition")
-    print("2. Substraction")
-    print("3. Multiplication")
-    print("4. Division")
-    print("0. Exit\n")
-    option = int(input("Enter choice: "))
-    if option == 0:
-        print("Program terminated.")
-        break
     a = float(input("Enter number 1: "))
     b = float(input("Enter number 2: "))
     match option:
